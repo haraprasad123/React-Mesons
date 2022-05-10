ff1fdc618acbe8d0bb5aa4df80807636b7c15979
Harsh Shah <hrshah211@gmail.com>
2022-05-10 17:01:18 -0400

    Code for Dynamic Menu

---
+++ src/components/contactUs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Info from './info'
+
+const contactUs = () => {
+  return (
+    <div>
+        <h1>Contact Us</h1>
+        <p>Hi I am Harsh Shah</p>
+        <Info />
+    </div>
+  )
+}
+
+export default contactUs
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+        <h1>Home</h1>
+        <p>Welcome to our Page please feel free to walk through</p>
+    </div>
+  )
+}
+
+export default home
